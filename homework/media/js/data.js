1fc8b7f18be950f634dfc83439d1466b9ed27f6a
Son Pham <hongsonnejii@gmail.com>
2020-08-07 09:55:53 +0700

    Up python

---
+++ homework/media/js/data.js
@@ -0,0 +1,115 @@
+let data = {
+    titlePage : 'Homework JS',
+    navMenu : [
+        {
+            name : 'Section 1 : Variable',
+            sub : [
+                {
+                    name : '1.1 - Homework',
+                    sub : [
+                        {
+                            name : 'Homework 1',
+                            link : 'Section-1/hwork-1/index.html'
+                        },
+                        {
+                            name : 'Homework 2',
+                            link : 'Section-1/hwork-2/index.html'
+                        },
+                        {
+                            name : 'Homework 3',
+                            link : 'Section-1/hwork-3/index.html'
+                        },
+                        {
+                            name : 'Homework 4',
+                            link : 'Section-1/hwork-4/index.html'
+                        },
+                        {
+                            name : 'Homework 5',
+                            link : 'Section-1/hwork-5/index.html'
+                        },
+                        {
+                            name : 'Homework 6',
+                            link : 'Section-1/hwork-6/index.html'
+                        },
+                        {
+                            name : 'Homework 7',
+                            link : 'Section-1/hwork-7/index.html'
+                        },
+                        {
+                            name : 'Homework 8',
+                            link : 'Section-1/hwork-8/index.html'
+                        },
+                        {
+                            name : 'Homework 9',
+                            link : 'Section-1/hwork-9/index.html'
+                        },
+                        {
+                            name : 'Homework 10',
+                            link : 'Section-1/hwork-10/index.html'
+                        },
+                    ]
+                },
+                {
+                    name : '1.2 - Video',
+                    sub : [
+                        {
+                            name : 'Homework 1',
+                            link : 'Section-1/hwork-1/'
+                        },
+                        {
+                            name : 'Homework 2',
+                            link : 'Section-1/hwork-2/'
+                        },
+                        {
+                            name : 'Homework 3',
+                            link : 'Section-1/hwork-3/'
+                        },
+                    ]
+                },
+               
+            ]
+        },
+        {
+            name : 'Section 2',
+            sub : [
+                {
+                    name : '2.1 - Homework',
+                    sub : [
+                        {
+                            name : 'Homework 1',
+                            link : 'Section-2/hwork-1/'
+                        },
+                        {
+                            name : 'Homework 2',
+                            link : 'Section-2/hwork-2/'
+                        },
+                        {
+                            name : 'Homework 3',
+                            link : 'Section-2/hwork-3/'
+                        },
+                    ]
+                },
+                {
+                    name : '2.2 - Video',
+                    sub : [
+                        {
+                            name : 'Homework 1',
+                            link : 'Section-2/hwork-1/'
+                        },
+                        {
+                            name : 'Homework 2',
+                            link : 'Section-2/hwork-2/'
+                        },
+                        {
+                            name : 'Homework 3',
+                            link : 'Section-2/hwork-3/'
+                        },
+                    ]
+                },
+               
+            ]
+        },
+              
+    ],
+
+}
